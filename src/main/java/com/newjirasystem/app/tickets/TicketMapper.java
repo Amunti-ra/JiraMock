e86d082687792e1a7e6c974702d4b20cc5003a21
Mati <matiasdmc@gmail.com>
2026-06-08 01:23:22 +0200

    feat: añadido TicketMapper  para conversion a TicketDTO
    Implementado toTicketDTO, pendiente fromTicketDTO

---
+++ src/main/java/com/newjirasystem/app/tickets/TicketMapper.java
@@ -0,0 +1,43 @@
+package com.newjirasystem.app.tickets;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketMapper {
+    /**
+     * Método toTicketDTO
+     *
+     * Recibe un ticket y lo transforma en un TicketDTO para poder pasarlo al TicketController con
+     * los datos que se quieren hacer visibles al front end.
+     *
+     * Revisa si está asignado a alguien, evitar NullPointerException.
+     *
+     * @param ticket entidad Ticket de la base de datos.
+     * @return TicketDTO con datos que se pasarán al front-end.
+     */
+    public TicketDTO toTicketDTO(Ticket ticket) {
+        if (ticket == null) {
+            return null;
+        }
+
+        String asignadoA = null;
+        if (ticket.getAsignado() != null) {
+            asignadoA = ticket.getAsignado().getNombre();
+        }
+
+        return new TicketDTO(
+                ticket.getId(),
+                ticket.getTitulo(),
+                ticket.getDescripcion(),
+                ticket.getClave(),
+                ticket.getProyecto().getNombre(),
+                (long) ticket.getTipo().getId(),
+                (long) ticket.getEstado().getId(),
+                (long) ticket.getPrioridad().getId(),
+                ticket.getCreador().getNombre(),
+                asignadoA,
+                ticket.getFechaCreacion()
+        );
+    }
+}
