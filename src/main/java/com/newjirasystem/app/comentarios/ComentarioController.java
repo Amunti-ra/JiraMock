e102f2614e167cbb5ba4250f9cb09f4e697e1432
Mati <matiasdmc@gmail.com>
2026-06-08 21:35:31 +0200

    feat(comentarios): añadir controlador para endpoints patch y delete
    - implementado endpoint PATCH para edición parcial de comentarios
    
    - implementado endpoint DELETE para eliminación de comentarios

---
+++ src/main/java/com/newjirasystem/app/comentarios/ComentarioController.java
@@ -0,0 +1,27 @@
+package com.newjirasystem.app.comentarios;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comentarios")
+public class ComentarioController {
+    private final ComentarioService comentarioService;
+
+    public ComentarioController(ComentarioService comentarioService) {this.comentarioService = comentarioService;}
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ComentarioDTO> patchComentario(@PathVariable Long id, @RequestBody ActualizarComentarioDTO dto) {
+        ComentarioDTO comentarioActualizado = comentarioService.patchComentario(id, dto);
+
+        return new ResponseEntity<>(comentarioActualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComentarioById(@PathVariable Long id) {
+        comentarioService.deleteComentario(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
