24e36c992cd485d66dc04f1f90f9b37ea3622e0f
Mati <matiasdmc@gmail.com>
2026-06-08 19:06:37 +0200

    feat(comentarios): crear ComentarioDTO
    - ID, texto, ticket, nombreAutor, fechaCreacion y fechaEditado

---
+++ src/main/java/com/newjirasystem/app/comentarios/ComentarioDTO.java
@@ -0,0 +1,11 @@
+package com.newjirasystem.app.comentarios;
+
+import java.time.LocalDateTime;
+
+public record ComentarioDTO(Long id,
+                            String texto,
+                            String ticket,
+                            String nombreAutor,
+                            LocalDateTime fechaCreacion,
+                            LocalDateTime fechaEditado) {
+}
