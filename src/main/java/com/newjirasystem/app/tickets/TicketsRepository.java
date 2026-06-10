323e715d9b863044ff486f404276e45fabe0937c
Mati <matiasdmc@gmail.com>
2026-06-10 18:59:57 +0200

    refactor(tickets): eliminar metodos redundantes de repositorio y extender JpaSpecificationExecutor
    - eliminados metodos de filtrado especificos en TicketsRepository en favor de filtros genericos con JpaSpecificationExecutor

--- src/main/java/com/newjirasystem/app/tickets/TicketsRepository.java
+++ src/main/java/com/newjirasystem/app/tickets/TicketsRepository.java
@@ -1,23 +1,17 @@
 package com.newjirasystem.app.tickets;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TicketsRepository extends JpaRepository<Ticket, Long> {
+public interface TicketsRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket> {
 
     List<Ticket> findAllByActivoTrue();
-
-    List<Ticket> findAllByProyectoIdAndActivoTrue(Long id);
-
-    List<Ticket> findAllByAsignadoIdAndActivoTrue(Long id);
-
-    List<Ticket> findAllByPrioridadAndActivoTrue(PrioridadTicket prioridad);
-
-    List<Ticket> findAllByEstadoAndActivoTrue(EstadoTicket estado);
 
     Optional<Ticket> findByIdAndActivoTrue(Long id);
 }
