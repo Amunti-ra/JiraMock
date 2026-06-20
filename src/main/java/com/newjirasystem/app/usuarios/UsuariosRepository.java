f58bacdc0464699bbc31f86c8f2404ecf31f10dc
Mati <matiasdmc@gmail.com>
2026-06-20 22:24:00 +0200

    feat(usuarios): añadir metodo findByNombre en UsuariosRepository
    - implementada consulta para buscar usuarios por nombre utilizando Optional

--- src/main/java/com/newjirasystem/app/usuarios/UsuariosRepository.java
+++ src/main/java/com/newjirasystem/app/usuarios/UsuariosRepository.java
@@ -3,6 +3,10 @@ package com.newjirasystem.app.usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByNombre(String name);
 }
