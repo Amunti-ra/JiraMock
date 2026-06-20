3d56e175212dc08bfdfb141b1740db69f2aca0ca
Mati <matiasdmc@gmail.com>
2026-06-21 00:24:47 +0200

    refactor(auth): marcar UserDetailsService como @Lazy en JwtAuthFilter
    - evitado el problema de referencia circular en la inyeccion de dependencias usando la anotacion @Lazy

--- src/main/java/com/newjirasystem/app/auth/JwtAuthFilter.java
+++ src/main/java/com/newjirasystem/app/auth/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,7 +21,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
-    public JwtAuthFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtAuthFilter(JwtService jwtService,@Lazy UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
     }
