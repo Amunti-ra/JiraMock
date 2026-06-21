403310dd55e81dcbb2f02f8e50c5d031cf6a8102
Mati <matiasdmc@gmail.com>
2026-06-22 00:47:35 +0200

    refactor(auth): eliminar clase SesionExpiradaException redundante en el sistema

--- src/main/java/com/newjirasystem/app/exception/SesionExpiradaException.java
+++
@@ -1,7 +0,0 @@
-package com.newjirasystem.app.exception;
-
-public class SesionExpiradaException extends RuntimeException {
-    public SesionExpiradaException() {
-        super("Tu sesión ha expirado, vuelve a hacer login");
-    }
-}
