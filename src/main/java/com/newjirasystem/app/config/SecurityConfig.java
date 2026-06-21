7b59ae4c6325789109216557ad8faacb88c43731
Mati <matiasdmc@gmail.com>
2026-06-21 23:11:18 +0200

    feat(config): permitir acceso publico a endpoints de Swagger
    - añadidos "/v3/api-docs/**", "/swagger-ui/**" y "/swagger-ui.html" como rutas permitidas sin autenticacion en SecurityConfig

--- src/main/java/com/newjirasystem/app/config/SecurityConfig.java
+++ src/main/java/com/newjirasystem/app/config/SecurityConfig.java
@@ -50,7 +50,12 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                                ).permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
