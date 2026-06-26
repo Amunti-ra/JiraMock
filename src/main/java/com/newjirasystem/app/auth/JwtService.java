ff1246f12b8e26ac380ecb5e0902d36701bdc568
Mati <matiasdmc@gmail.com>
2026-06-26 18:00:48 +0200

    refactor(auth): mejorar gestion de expiracion y configuracion en JwtService
    - inyectada dependencia JwtProperties y Clock para manejar propiedades y tiempo en JWT
    
    - eliminada logica redundante de verificacion de expiracion en tokens
    
    - renombrados metodos esTokenValido y esRefreshTokenValido para mayor claridad

--- src/main/java/com/newjirasystem/app/auth/JwtService.java
+++ src/main/java/com/newjirasystem/app/auth/JwtService.java
@@ -1,50 +1,55 @@
 package com.newjirasystem.app.auth;
 
+import com.newjirasystem.app.config.JwtProperties;
 import com.newjirasystem.app.usuarios.Usuario;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.time.Clock;
+import java.time.Instant;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
     private final SecretKey secretKey;
-    private final int tiempoExpiracion;
-    private final int tiempoExpiracionRefreshToken;
+    private final Clock clock;
+    private final JwtProperties jwtProperties;
 
-    public JwtService(@Value("${app.jwt.secret}") String secretKey, @Value("${app.jwt.expiration-time}") int tiempoExpiracion, @Value("${app.jwt.refresh-expiration-time}") int tiempoExpiracionRefreshToken) {
-        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-        this.tiempoExpiracion = tiempoExpiracion;
-        this.tiempoExpiracionRefreshToken = tiempoExpiracionRefreshToken;
+    public JwtService(JwtProperties jwtProperties, Clock clock) {
+        this.jwtProperties = jwtProperties;
+        this.clock = clock;
+        this.secretKey = Keys.hmacShaKeyFor(jwtProperties.secret().getBytes(StandardCharsets.UTF_8));
     }
 
     public String generarToken(Usuario usuario) {
-        return buildToken(usuario, tiempoExpiracion, "access_token");
+        return buildToken(usuario, jwtProperties.expirationInMs(), "access_token");
     }
 
     public String generarRefreshToken(Usuario usuario) {
-        return buildToken(usuario, tiempoExpiracionRefreshToken, "refresh_token");
+        return buildToken(usuario, jwtProperties.refreshExpirationInMs(), "refresh_token");
     }
 
     private String buildToken(Usuario usuario, int expiracion, String tipo) {
+        Instant ahora = clock.instant();
+        Instant fechaExpiracion = ahora.plusMillis(expiracion);
+
         return Jwts.builder()
                 .subject(usuario.getNombre())
                 .claim("rol", usuario.getRol().toString())
                 .claim("type", tipo)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiracion))
+                .issuedAt(Date.from(ahora))
+                .expiration(Date.from(fechaExpiracion))
                 .signWith(secretKey)
                 .compact();
     }
 
-    public boolean esTokenValido(String token, UserDetails userDetails) {
+    public boolean esTokenVerificado(String token, UserDetails userDetails) {
         try {
             Claims claims = obtenerClaims(token);
 
@@ -55,27 +60,24 @@ public class JwtService {
                             .equals("ROLE_" + claims.get("rol")));
 
 
-            boolean esTokenExpirado = estaVigente(token);
-
-            return esUsuarioValido && esRolValido && esTokenExpirado && esTipoAccessToken;
+            return esUsuarioValido && esRolValido && esTipoAccessToken;
 
         } catch (Exception e) {
             return false;
         }
     }
 
-    public boolean esRefreshTokenValido(String token, UserDetails usuario) {
+    public boolean esRefreshTokenVerificado(String token, UserDetails usuario) {
         try {
             Claims claims = obtenerClaims(token);
 
             boolean esTipoRefreshToken = claims.get("type").equals("refresh_token");
             boolean esUsuarioValido = usuario.getUsername().equals(claims.getSubject());
-            boolean estaVigente = estaVigente(token);
             boolean esRolValido = usuario.getAuthorities().stream()
                     .anyMatch(a -> a.getAuthority()
                             .equals("ROLE_" + claims.get("rol")));
 
-            return esUsuarioValido && esRolValido && estaVigente && esTipoRefreshToken;
+            return esUsuarioValido && esRolValido && esTipoRefreshToken;
 
         } catch (Exception e) {
             return false;
@@ -86,15 +88,10 @@ public class JwtService {
         return obtenerClaims(token).getSubject();
     }
 
-    private boolean estaVigente(String token) {
-        Claims claims = obtenerClaims(token);
-
-        return claims.getExpiration().after(new Date());
-    }
-
     private Claims obtenerClaims(String token) {
         return Jwts.parser()
                 .verifyWith(secretKey)
+                .clock(()-> Date.from(clock.instant()))
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
