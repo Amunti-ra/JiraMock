7fb917ca0e8cf3032ca54dea5714c078d8d5726b
Mati <matiasdmc@gmail.com>
2026-09-08 18:06:06 +0200

    feat(comentarios): añadir anotacion @Repository en ComentarioRepository
    - permite deteccion automatica de la interfaz como un componente Spring

--- src/main/java/com/newjirasystem/app/comentarios/ComentarioRepository.java
+++ src/main/java/com/newjirasystem/app/comentarios/ComentarioRepository.java
@@ -1,10 +1,12 @@
 package com.newjirasystem.app.comentarios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
 
     List<Comentario> findAllByTicketIdAndActivoTrue(Long id);
