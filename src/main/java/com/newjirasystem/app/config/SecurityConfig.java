78bf5332890905087fb876b015dc428d2d3ca259
Mati <matiasdmc@gmail.com>
2026-09-08 18:04:18 +0200

    feat(config): habilitar seguridad basada en metodos en SecurityConfig
    - añadido @EnableMethodSecurity para permitir el uso de anotaciones como @PreAuthorize en controladores y servicios

--- src/main/java/com/newjirasystem/app/config/SecurityConfig.java
+++ src/main/java/com/newjirasystem/app/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -35,6 +36,7 @@ import java.time.Clock;
         bearerFormat = "JWT"
 )
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UsuariosRepository usuariosRepository;
