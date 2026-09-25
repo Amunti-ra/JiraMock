3c679071020d58abe3ec4814fa1132a3b93af350
Mati <matiasdmc@gmail.com>
2026-09-25 23:39:17 +0200

    test: eliminar configuracion innecesaria de AutoConfigureTestDatabase en pruebas de repositorios
    - remover anotacion @AutoConfigureTestDatabase en ComentarioRepositoryTest y TicketsRepositoryTest para evitar conflictos con configuraciones de base de datos

--- src/test/java/com/newjirasystem/app/comentarios/ComentarioRepositoryTest.java
+++ src/test/java/com/newjirasystem/app/comentarios/ComentarioRepositoryTest.java
@@ -18,7 +18,6 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ComentarioRepositoryTest {
 
     @Autowired
--- src/test/java/com/newjirasystem/app/tickets/TicketsRepositoryTest.java
+++ src/test/java/com/newjirasystem/app/tickets/TicketsRepositoryTest.java
@@ -17,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TicketsRepositoryTest {
 
     @Autowired
