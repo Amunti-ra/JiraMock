6a51aa8de85b1dd1fdd8038910fd8faf86e5839f
Mati <matiasdmc@gmail.com>
2026-09-25 23:46:14 +0200

    test: agregar perfil de prueba en tests de repositorios
    - incluir anotacion @ActiveProfiles("test") en ComentarioRepositoryTest y TicketsRepositoryTest para garantizar configuracion especifica en pruebas

--- src/test/java/com/newjirasystem/app/comentarios/ComentarioRepositoryTest.java
+++ src/test/java/com/newjirasystem/app/comentarios/ComentarioRepositoryTest.java
@@ -10,11 +10,14 @@ import com.newjirasystem.app.usuarios.UsuariosRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class ComentarioRepositoryTest {
 
     @Autowired
